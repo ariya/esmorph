a68289b3c00d9e3bef05d53d8b4c17749b6ca78c
Ariya Hidayat <ariya.hidayat@gmail.com>
2013-05-14 23:56:26 -0700

    Add a test for multiple functions.

---
+++ test/data/multiple.js
@@ -0,0 +1,8 @@
+function foo() {}
+
+function bar() {
+
+    console.log('Hello');
+
+    function baz() { alert('baz') }
+}
---
+++ test/data/multiple_morphed.js
@@ -0,0 +1,11 @@
+function foo() {
+Enter({ name: 'foo', lineNumber: 1, range: [0, 17] });}
+
+function bar() {
+Enter({ name: 'bar', lineNumber: 3, range: [19, 101] });
+
+    console.log('Hello');
+
+    function baz() {
+Enter({ name: 'baz', lineNumber: 7, range: [68, 99] }); alert('baz') }
+}
